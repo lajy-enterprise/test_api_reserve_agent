2c811bfc3aff6bfed4c7c623e89c1e8519a540c1
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-11-04 14:29:02 -0300

    cleaned logs

--- src/FirebaseDB/firebase.admin.ts
+++ src/FirebaseDB/firebase.admin.ts
@@ -1,20 +1,9 @@
 import { initializeApp, cert } from "firebase-admin/app";
-// import { getDatabase } from "firebase-admin/database";
+
 import { getFirestore } from "firebase-admin/firestore";
 import * as dotenv from "dotenv";
 
-import fs from "fs";
-import * as path from "path";
-
 dotenv.config();
-
-// const filePath = path.join(
-//   __dirname,
-//   "agentar-da00f-firebase-adminsdk-75npg-a1016f6b4d.json"
-// );
-
-// const jsonFile = fs.readFileSync(filePath, "utf-8");
-// const serviceAccount = JSON.parse(jsonFile);
 
 const app = initializeApp({
   credential: cert({
@@ -24,14 +13,4 @@ const app = initializeApp({
   }),
 });
 
-console.log(process.env.PROJECT_ID);
-console.log("-----------------");
-
-console.log(process.env.CLIENT_EMAIL);
-console.log("-----------------");
-
-console.log(process.env.PRIVATE_KEY);
-
 export const firestoreDB = getFirestore(app);
-
-// const app = initializeApp();
